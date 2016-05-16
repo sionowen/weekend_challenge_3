ef5a1285608bd265f792b03f1345ed433acedb93
sionowen <sionowen34@gmail.com>
2016-05-16 13:33:06 -0500

    completed hard mode

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,29 +1,45 @@
-$(document).on('ready', function(){
+var values = {};
 
-  $('.inputButton').on('click', function(){
+$(document).on('ready', function() {
+	$('button').on('click', function() {
+		event.preventDefault();
+	});
+
+  $('.numSelector').on('click', function(){
+    var addText = $(this).text();
+    console.log(addText);
+    $('#displayedNum').val($('#displayedNum').val() + addText);
+  });
+
+	$('.inputButton').on('click', function() {
+		event.preventDefault();
+    values.firstNum = $('#displayedNum').val();
+		values.operation = this.name;
+    $('#displayedNum').val('');
+		console.log(values);
+	})
+
+  $('.equalButton').on('click', function(){
     event.preventDefault();
-    var values = {};
-    $.each($('#calculator').serializeArray(), function(i, field){
-      values[field.name] = field.value;
-
-    });
-    values.operation = this.name;
+    values.secondNum = $('#displayedNum').val();
     console.log(values);
-
     $.ajax({
       type: 'POST',
       url: '/operation/' + values.operation,
       data: values,
-      success: function(response){
+      success: function(response) {
         console.log(response);
         $('.result').text(response);
+        $('#displayedNum').val(response);
       }
-    })
+    });
+    // $('#calculator').trigger("reset");
   })
-  $('.clearForm').on('click', function(){
-    event.preventDefault();
-    $('#calculator').trigger("reset");
 
-  })
+	$('.clearForm').on('click', function() {
+		event.preventDefault();
+		$('#calculator').trigger("reset");
+
+	})
 
 })
--- server/routes/operation.js
+++ server/routes/operation.js
@@ -2,22 +2,22 @@ var express = require('express');
 var router = express.Router();
 
 router.post('/plus', function(req, res) {
-  console.log(req.body);
-  res.send(Number(req.body.firstNum) + Number(req.body.secondNum) + '');
+	console.log(req.body);
+	res.send(Number(req.body.firstNum) + Number(req.body.secondNum) + '');
 })
 
 router.post('/minus', function(req, res) {
-  console.log(req.body);
-  res.send(Number(req.body.firstNum) - Number(req.body.secondNum) + '');
+	console.log(req.body);
+	res.send(Number(req.body.firstNum) - Number(req.body.secondNum) + '');
 })
 
 router.post('/multiply', function(req, res) {
-  console.log(req.body);
-  res.send(Number(req.body.firstNum) * Number(req.body.secondNum) + '');
+	console.log(req.body);
+	res.send(Number(req.body.firstNum) * Number(req.body.secondNum) + '');
 })
 
 router.post('/divide', function(req, res) {
-  console.log(req.body);
-  res.send(Number(req.body.firstNum) / Number(req.body.secondNum) + '');
+	console.log(req.body);
+	res.send(Number(req.body.firstNum) / Number(req.body.secondNum) + '');
 })
 module.exports = router
